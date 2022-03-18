cc248dac64b9f5fd649bfd864efd3541499ea30a
Jonathan Santos <jonathan01347@gmail.com>
2022-03-18 16:40:57 -0300

    exercicio 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const adicinaAnimal = species.reduce((acc, item) => {
+      acc[item.name] = item.residents.length;
+      return acc;
+    }, {});
+    return adicinaAnimal;
+  }
+  const { specie, sex } = animal;
+  if (Object.keys(animal).includes('sex')) {
+    const getSex = species.filter((elemento) => elemento.name === specie);
+    return getSex.find((elemento1) => elemento1.residents).residents
+      .filter((elemento2) => elemento2.sex === sex).length;
+  }
+
+  if (Object.keys(animal).includes('specie')) {
+    const getAmount = species.find((listAnimal) => listAnimal.name === specie).residents.length;
+    return getAmount;
+  }
 }
+console.log(countAnimals());
 
 module.exports = countAnimals;
