0167e7550b3db66c6482e57bf5eacee3b307cac6
Jonathan Santos <jonathan01347@gmail.com>
2022-03-19 05:47:56 -0300

    exercicio 6 feito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-
+  const testObjeto = {
+    child: entrants.filter((elemento) => elemento.age < 18).length,
+    adult: entrants.filter((elemento1) => elemento1.age >= 18 && elemento1.age < 50).length,
+    senior: entrants.filter((elemento3) => elemento3.age >= 50).length,
+  };
+  return testObjeto;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+
+  const child = entrants.filter((elem) => elem.age < 18).length * prices.child;
+  const adult = entrants.filter((elem1) => elem1.age >= 18 && elem1.age < 50).length * prices.adult;
+  const senior = entrants.filter((elem3) => elem3.age >= 50).length * prices.senior;
+  const soma = child + adult + senior;
+  return soma;
 }
 
 module.exports = { calculateEntry, countEntrants };
